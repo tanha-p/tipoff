ae25e3d68e1148d47f9f44596425a39c92258aa7
tanha-p <tpatoliya@gmail.com>
2020-05-20 21:28:17 -0500

    added apidoc for add-tip api

--- api-server/routes/add-tip/add-tip-route.js
+++ api-server/routes/add-tip/add-tip-route.js
@@ -11,8 +11,29 @@ const log = log4js.getLogger("tips-route-ctrl");
 const router = express.Router();
 const tipsSvc = new TipsSvc();
 
-/* GET Tips API.  
-/api/v1/tips(?pageNo=&items=)
+/** 
+@api {post} /api/v1/add-tip
+@apiName AddTip
+@apiGroup Add Tip From Browser
+
+@apiParam {String} projectId This is the eventType that is associated with each Tip. 
+@apiParam {String} eventType Tips with eventType such as success, info, error, unhandled or warning get highlighted 
+with appropriate color on the View Tips page. eventType is not limited to only 
+these types. Any custom eventType string is acceptable.
+@apiParam {String} eventTitle The purpose of this field is to provide human 
+readable, meaningful title to the event.
+@apiParam {String[]} [tags] This is an array of strings and can be used to pass 
+different tags to the Tip and can help combine similar Tips while searching. e.g. All events in the checkout flow can be tagged with 'checkout' tag and can later be searched using the same tag.
+@apiParam {Object} [user] This can be used to pass the user object associated 
+with the event. Passing user.user_id as a unique identifier is highly recommended
+ when passing user object. user_id field is searchable. 
+ Rest of the user object is not searchable and can be used to search all the 
+ Tips triggerred by the user_id.
+@apiParam {String} [note] This field can be used to pass any kind of note related 
+ to the Tip that will help the analysis later on.
+@apiParam {Object} [eventStack[stackTrace]] stacktrace of generated error
+@apiParam {String} [screenshot] data:image (base64 encoded string) of the screenshot taken of user's browser
+
 */
 router.post('/',
     validateOriginForProject, //middleware to validate if origin is allowed to send request
@@ -54,7 +75,6 @@ router.post('/',
              * notifications are sent.
              */ 	
 			tipsSvc.addTip(req);
-			// 	.catch(error => { throw error; });
 			return RouteUtils.sendSuccessResponse(res,`Tip added successfully.`, 201,  log, 
 			 	`Tip added successfully.`,req.id);
 		} catch(error) {
